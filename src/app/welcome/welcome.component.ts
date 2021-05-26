8db5538bc82d49607cfec70f928b1a9ed9dcb2dd
emeban44 <emi.cazorla@hotmail.com>
2021-05-26 17:57:17 +0200

    http get request working

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,5 +1,7 @@
+import { HttpResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { WelcomeDataService } from '../service/data/welcome-data.service';
 // import { AppComponent } from '../app.component';
 
 @Component({
@@ -10,20 +12,32 @@ import { ActivatedRoute } from '@angular/router';
 export class WelcomeComponent implements OnInit {
 
   message : string = 'Some Welcome Message'
+  welcomeMessageFromService : string | undefined
   name = ''
 
   //ActivatedRoute
-  constructor(private route: ActivatedRoute) { }
+  constructor(
+    private route: ActivatedRoute,
+    private service: WelcomeDataService) { }
 
   ngOnInit(): void {
     console.log(this.message)
     this.name = this.route.snapshot.params['name']
   }
 
-}
+  getWelcomeMessage(): void {
+    console.log(this.service.executeHelloWorldBeanService());
+    this.service.executeHelloWorldBeanService().subscribe(
+      response => this.handleSuccessFulResponse(response)
+    );
 
-export class Class1 {
-  constructor() {
-    
+    console.log('last line of get Welcome Message')
   }
+
+  handleSuccessFulResponse(response: any){
+    this.welcomeMessageFromService = response.message;
+   // console.log(response);
+    console.log(response.message);
+  }
+
 }
